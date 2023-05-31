ae3d2f15244cb79639f2c414a20f1f2cf951bcd1
ayush13shabu <anyush67@gmail.com>
2023-05-31 23:13:38 +0530

    Initial commit make a simple no function notes app and add sample hardcoded notes

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+    <div className = "note">
+        <h1>Ayush</h1>
+        <p>Ansh</p>
+    </div>
+    );
+}
+
+export default Note;
