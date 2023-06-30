4e98315d80b9cb5d319cd5721d6063dfba40f969
ayush13shabu <anyush67@gmail.com>
2023-06-30 18:12:12 +0530

    Handle events on title and content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  return (
+    const [items, setItems] = useState([]);
+    
+    function addItem(title){
+
+        return 
+    }
+
+return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addItem}/>
+      <Note 
+      key={1} 
+      title="Note title" 
+      content="Note content" />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return{
+                ...prevNote,
+                [name]: value
+            }
+        }
+        )
+    }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" 
+        type="text"
+        placeholder="Title" 
+        onChange={handleChange}
+        value={note.title}
+        />
+
+        <textarea name="content" 
+        type="text" 
+        onChange={handleChange}
+        value={note.content}
+        placeholder="Take a note..." 
+        rows="3" />
+
+        <button
+        onClick={() => {
+          props.onAdd(note);
+          setNote("")
+        }}>Add</button>
       </form>
     </div>
   );
 }
 
 export default CreateArea;
+
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
