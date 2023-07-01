ab95da0b42c1e23ee4f1e66e0faef63f7315741f
ayush13shabu <anyush67@gmail.com>
2023-07-01 19:49:34 +0530

    Add new styles to input class and reformat code

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,58 +1,51 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
 
-    const [note, setNote] = useState({
-        title: "",
-        content: ""
-    })
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    function handleChange(event) {
-        const {name, value} = event.target;
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
 
-        setNote(prevNote => {
-            return{
-                ...prevNote,
-                [name]: value
-            }
-        }
-        )
-    }
-
-    function submitNote(event){
-        props.onAdd(note)
-        event.preventDefault()
-        setNote(
-            {
-                title: "",
-                content: ""
-            }
-        )
-    }
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
-      <form>
-        <input name="title" 
-        type="text"
-        placeholder="Title" 
-        onChange={handleChange}
-        value={note.title}
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
         />
-
-        <textarea name="content" 
-        type="text" 
-        onChange={handleChange}
-        value={note.content}
-        placeholder="Take a note..." 
-        rows="3" />
-
-        <button
-        onClick={submitNote}>Add</button>
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
 }
 
 export default CreateArea;
-
